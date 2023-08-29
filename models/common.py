56a5d658c0a2194a9c2465de4e9882fe8f2a5258
Sheeva <shivagh1372@gmail.com>
2023-08-29 11:41:33 +0330

    added common.py to readme file.

--- models/common.py
+++ models/common.py
@@ -3,7 +3,7 @@ from playwright.sync_api import expect
 
 class CommonTools:
     def __init__(self, page):
-        self.page = page 
+        self.page = page
 
     def check_page_is_loaded(self, page_title):
         title = self.page.get_by_text(page_title)
