2e7ec91e64fe76deda189de8a762f7b15e6ab79c
Sheeva <shivagh1372@gmail.com>
2023-09-02 13:47:48 +0330

    added two functions in bookmark model file.

--- models/bookmark.py
+++ models/bookmark.py
@@ -2,7 +2,13 @@ class Bookmark:
     def __int__(self, page):
         self.page = page
 
+    # user can find product in two ways,directly and un-directly.first method attempts to find product by searching in search bar
+    # and in second one, user tries to reach it by scrolling and click on brands etc.
+    def find_product_by_search_bar(self):
 
-    def find_product_in_brand(self):
+
+
+
+    def find_product_in_brand_list(self):
 
 
