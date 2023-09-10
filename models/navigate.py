86a3642430fbb0cdd1ef026905714aed9e8eb442
shivaghanbari <shivagh1372@gmail.com>
2023-09-10 22:19:37 +0330

    added comment in navigate.py file.

--- models/navigate.py
+++ models/navigate.py
@@ -1,3 +1,6 @@
+# This function is used for intercepting network requests and making decisions based on the request URL.
+# If the request URL contains the word "google," it will block the request and print a message indicating
+# that it's being blocked. Otherwise, it allows the request to continue.
 def rout_intercept(route):
     if "google" in route.request.url:
         print(f"blocking {route.request.url} as it contains Google")
